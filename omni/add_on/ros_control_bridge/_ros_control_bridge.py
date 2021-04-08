4ddb6864420c8851f871436c8df5a58e77d59c9b
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2021-04-08 21:25:28 +0000

    Create ROS Control bridge interface

---
+++ omni/add_on/ros_control_bridge/_ros_control_bridge.py
@@ -0,0 +1,11 @@
+
+
+def acquire_roscontrolbridge_interface(plugin_name=None, library_path=None):
+    return RosControlBridge() 
+
+def release_roscontrolbridge_interface(bridge):
+    bridge
+
+class RosControlBridge:
+    pass
+
--- omni/add_on/ros_control_bridge/scripts/extension.py
+++ omni/add_on/ros_control_bridge/scripts/extension.py
@@ -1,5 +1,6 @@
 import omni.ext
 
+from .. import _ros_control_bridge
 from .menu import RosControlBridgeMenu
 
 EXTENSION_NAME = "ROS Control Bridge"
@@ -7,8 +8,10 @@ EXTENSION_NAME = "ROS Control Bridge"
 
 class Extension(omni.ext.IExt):
     def on_startup(self):
+        self._roscontrolbridge = _ros_control_bridge.acquire_roscontrolbridge_interface()
         self._menu = RosControlBridgeMenu()
 
     def on_shutdown(self):
+        _ros_control_bridge.release_roscontrolbridge_interface(self._roscontrolbridge)
         self._menu.shutdown()
         self._menu = None
