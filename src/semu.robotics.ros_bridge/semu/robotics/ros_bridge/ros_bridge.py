614e4f5c9aecb33dabe1aa8237f07fbe26b28750
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-06-14 21:35:42 +0200

    Init ROS node during simulation event

--- src/semu.robotics.ros_bridge/semu/robotics/ros_bridge/ros_bridge.py
+++ src/semu.robotics.ros_bridge/semu/robotics/ros_bridge/ros_bridge.py
@@ -5,9 +5,10 @@ import rospy
 import rosgraph
 
 
+_ROS_NODE_RUNNING = False
+
 def acquire_ros_bridge_interface(ext_id: str = "") -> 'RosBridge':
-    """
-    Acquire the RosBridge interface
+    """Acquire the RosBridge interface
 
     :param ext_id: The extension id
     :type ext_id: str
@@ -18,13 +19,33 @@ def acquire_ros_bridge_interface(ext_id: str = "") -> 'RosBridge':
     return RosBridge()
 
 def release_ros_bridge_interface(bridge: 'RosBridge') -> None:
-    """
-    Release the RosBridge interface
+    """Release the RosBridge interface
     
     :param bridge: The RosBridge interface
     :type bridge: RosBridge
     """
     bridge.shutdown()
+
+def is_ros_master_running() -> bool:
+    """Check if the ROS master is running
+
+    :returns: True if the ROS master is running, False otherwise
+    :rtype: bool
+    """
+     # check ROS master
+    try:
+        rosgraph.Master("/rostopic").getPid()
+    except:
+        return False
+    return True
+
+def is_ros_node_running() -> bool:
+    """Check if the ROS node is running
+
+    :returns: True if the ROS node is running, False otherwise
+    :rtype: bool
+    """
+    return _ROS_NODE_RUNNING
 
 
 class RosBridge:
@@ -40,30 +61,25 @@ class RosBridge:
         self._timeline_event = self._timeline.get_timeline_event_stream().create_subscription_to_pop(self._on_timeline_event)
 
         # ROS node
-        self._init_ros_node()
+        if is_ros_master_running():
+            self._init_ros_node()
 
     def shutdown(self) -> None:
         """Shutdown the RosBridge interface
         """
         self._timeline_event = None
+        rospy.signal_shutdown("semu.robotics.ros_bridge shutdown")
 
     def _init_ros_node(self) -> None:
+        global _ROS_NODE_RUNNING
         """Initialize the ROS node
         """
-        # check ROS master
         try:
-            rosgraph.Master("/rostopic").getPid()
-        except:
-            print("[Warning][semu.robotics.ros_bridge] {}: ROS master is not running".format(self._node_name))
-            return False
-        # start ROS node
-        try:
-            rospy.init_node(self._node_name)
+            rospy.init_node(self._node_name, disable_signals=False)
             print("[Info][semu.robotics.ros_bridge] {} node started".format(self._node_name))
         except rospy.ROSException as e:
             print("[Error][semu.robotics.ros_bridge] {}: {}".format(self._node_name, e))
-            return False
-        return True
+        _ROS_NODE_RUNNING = True
 
     def _on_timeline_event(self, event: 'carb.events._events.IEvent') -> None:
         """Handle the timeline event
@@ -72,7 +88,8 @@ class RosBridge:
         :type event: carb.events._events.IEvent
         """
         if event.type == int(omni.timeline.TimelineEventType.PLAY):
-            pass
+            if is_ros_master_running():
+                self._init_ros_node()
         elif event.type == int(omni.timeline.TimelineEventType.PAUSE):
             pass
         elif event.type == int(omni.timeline.TimelineEventType.STOP):
